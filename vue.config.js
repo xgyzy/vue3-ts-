9632ba79f9e3522294ffaea94b0e91b661ba0041
xgyzy <646915778@qq.com>
2022-03-03 16:13:12 +0800

    代码规范配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  outputDir: './build',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: '@/views',
+        components: '@/components'
+      }
+    }
+  }
+})
